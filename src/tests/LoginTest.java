28e17410249e3f698632d9ccf2062d2ef854946d
prashantqaengg <qaengg.prashantchavan@gmail.com>
2022-01-26 18:50:25 +0530

    verify page titile

--- src/tests/LoginTest.java
+++ src/tests/LoginTest.java
@@ -5,5 +5,6 @@ public class LoginTest {
 	
 	public void doLogin(){
 		System.out.println("Successfully login to the aplication with valid user name and password.");
+		System.out.println("Verify page title after login");
 	}
 }
