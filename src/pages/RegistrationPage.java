181bfcb6153265d3967d1d907a17de38c69e921f
prashantqaengg <qaengg.prashantchavan@gmail.com>
2022-01-27 10:11:08 +0530

    new files are added in develope branch

--- src/pages/RegistrationPage.java
+++ src/pages/RegistrationPage.java
@@ -2,4 +2,8 @@ package pages;
 
 public class RegistrationPage {
 
+	private void doRegistration() {
+		System.out.println("RegistrationPage");
+
+	}
 }
---
+++ src/tests/RegistrationTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class RegistrationTest {
+
+	
+	public void createUser(){
+		System.out.println("Create user before login to application.");
+	}
+}
