2a53773d569ed6ed64414d792840d1564871fec7
prashantqaengg <qaengg.prashantchavan@gmail.com>
2022-01-27 13:45:27 +0530

    feature three

--- src/pages/FeatureOne.java
+++ src/pages/FeatureOne.java
@@ -5,5 +5,6 @@ public class FeatureOne {
     public static void main(String[] args) {
         System.out.println("Feature one is ready");
         System.out.println("Feature two is ready");
+        System.out.println("Feature three is ready");
     }
 }
