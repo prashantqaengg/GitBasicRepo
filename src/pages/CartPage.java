3bd1c8bf2ba5be3b6f901cb7ae2700919fc1d501
prashantqaengg <qaengg.prashantchavan@gmail.com>
2022-01-28 10:43:46 +0530

    resolved the conflicts

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -3,8 +3,8 @@ package pages;
 public class CartPage {
 
     public void addProductIntoCart(){
-        String custname="D5";
-        System.out.println("Product is added in cart"+custname);
+        String name="D7";
+        System.out.println("Product is added in cart"+name);
     }
 
 }
