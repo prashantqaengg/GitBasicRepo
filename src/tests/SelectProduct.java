1ed7ca6d7406fcc9fa337b3e76e2c4fc355e686d
Prashant Chavan <98458427+prashantqaengg@users.noreply.github.com>
2022-01-28 12:05:25 +0530

    Create SelectProduct.java

---
+++ src/tests/SelectProduct.java
@@ -0,0 +1,13 @@
+package tests;
+
+
+public class SelectProduct {
+
+    public void filterProduct(){
+        String productName="Mouse;
+       System.out.prinln("Selected product is "productName)
+    }
+    
+    
+    
+}
