01b548f3eb3ebb28dfc4d6c7fb92ec7d6b595fd3
Prashant Chavan <98458427+prashantqaengg@users.noreply.github.com>
2022-01-28 10:38:06 +0530

    Update CartPage.java

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -3,8 +3,6 @@ package pages;
 public class CartPage {
 
     public void addProductIntoCart(){
-        String productName="SamsungMobile";
-        String price="200INR";
         String custname="D5";
         System.out.println("Product is added in cart"+custname);
     }
