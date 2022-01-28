c7e01d871b692793bcc2fa170b168f1d7b7cf1a8
Prashant Chavan <98458427+prashantqaengg@users.noreply.github.com>
2022-01-28 11:44:58 +0530

    Update MakePayment.java

--- src/tests/MakePayment.java
+++ src/tests/MakePayment.java
@@ -3,7 +3,7 @@ package tests;
 public class MakePayment {
 
     public void doCardPayment(){
-        String amt="2000";
+        String Amount="2000";
         System.out.println("Payment is done::"+amt);
     }
 
